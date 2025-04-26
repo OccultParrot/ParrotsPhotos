89f382ee89e5cf08069ccca5a5218ec7a43813bf
Thomas Stemler <stemlertho@gmail.com>
2025-04-26 13:12:14 -0500

    Started Router, and made homepage element

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,10 +1,30 @@
-function App() {
+import { BrowserRouter, Outlet, Route, Routes } from "react-router-dom";
+import {ReactElement} from "react";
+import Home from "./pages/Home.tsx";
 
-  return (
-    <>
-      <p className="text-2xl">Hello World</p>
-    </>
-  )
+
+
+function App() {
+	const Layout = () => {
+		return (
+			<>
+				<main className="grow bg-white">
+					<Outlet />
+				</main>
+			</>
+		)
+	}
+
+
+	return (
+		<BrowserRouter>
+			<Routes>
+				<Route path="/" element={<Layout />}>
+					<Route index element={<Home />} />
+				</Route>
+			</Routes>
+		</BrowserRouter>
+	)
 }
 
 export default App
---
+++ client/src/pages/Home.tsx
@@ -0,0 +1,8 @@
+export default function Home() {
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center bg-gray-100">
+			<h1 className="text-4xl font-bold text-gray-800">Welcome to the Home Page</h1>
+			<p className="mt-4 text-lg text-gray-600">This is a simple home page built with React and Tailwind CSS.</p>
+		</div>
+	);
+}
