766f45ce154917394bda325c0be0e185a5d17c5d
Thomas Stemler <stemlertho@gmail.com>
2025-04-26 21:09:10 -0500

    Added temporary stuff for the about and main pages

--- client/src/pages/About.tsx
+++ client/src/pages/About.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center">
-			<h1 className="text-4xl font-bold text-gray-800">Welcome to the Home Page</h1>
-			<p className="mt-4 text-lg text-gray-600">This will be the starting point for people entering the website</p>
+			<h1 className="text-4xl font-bold text-gray-800">About Page</h1>
+			<p className="mt-4 text-lg text-gray-600">This will hold information about things like what cameras I use, etc.</p>
 		</div>
 	);
 }
