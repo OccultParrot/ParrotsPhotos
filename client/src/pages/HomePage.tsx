8a30348a58aa5a2bd7e1fc3b588ddde3eb5051fa
Thomas Stemler <stemlertho@gmail.com>
2025-04-28 15:43:18 -0500

    Updated page to use new colors

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,8 +1,8 @@
 export default function HomePage() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center">
-			<h1 className="text-4xl font-bold text-gray-800">Home Page</h1>
-			<p className="mt-4 text-lg text-gray-600">This will be the starting point for people entering the website</p>
+			<h1 className="text-4xl font-bold text-[var(--text-primary)]">Home Page</h1>
+			<p className="mt-4 text-lg text-[var(--text-secondary)]">This will be the starting point for people entering the website</p>
 		</div>
 	);
 }
