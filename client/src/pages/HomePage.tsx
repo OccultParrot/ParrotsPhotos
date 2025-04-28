5a3e32db78380c355d142f5dd8b87e56b8939183
Thomas Stemler <stemlertho@gmail.com>
2025-04-27 21:11:06 -0500

    Renamed page files so that they say name in their files

--- client/src/pages/HomePage.tsx
+++ client/src/pages/HomePage.tsx
@@ -1,4 +1,4 @@
-export default function Home() {
+export default function HomePage() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center">
 			<h1 className="text-4xl font-bold text-gray-800">Home Page</h1>
---
+++ client/src/pages/PhotosPage.tsx
@@ -0,0 +1,8 @@
+export default function PhotosPage() {
+	return (
+		<div className="flex min-h-screen flex-col items-center justify-center">
+			<h1 className="text-4xl font-bold text-gray-800">Photos Page</h1>
+			<p className="mt-4 text-lg text-gray-600">This page holds all my photos</p>
+		</div>
+	);
+}
