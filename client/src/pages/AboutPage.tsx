856a7f971434a044f074b83a2a8efb715924ea5d
Thomas Stemler <stemlertho@gmail.com>
2025-04-28 15:58:18 -0500

    Updated ALL pages to reflect new theme

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,8 +1,8 @@
 export default function Home() {
 	return (
 		<div className="flex min-h-screen flex-col items-center justify-center">
-			<h1 className="text-4xl font-bold text-gray-800">About Page</h1>
-			<p className="mt-4 text-lg text-gray-600">This will hold information about things like what cameras I use, etc.</p>
+			<h1 className="text-4xl font-bold text-[var(--text-primary)]">About Page</h1>
+			<p className="mt-4 text-lg text-[var(--text-secondary)]">This will hold information about things like what cameras I use, etc.</p>
 		</div>
 	);
 }
